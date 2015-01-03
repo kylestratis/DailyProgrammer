2178838d0f240cee1cee69aaaa7cf5d1beb99fd5
Kyle Stratis <kyle.stratis@gmail.com>
2015-01-03 13:54:16 -0500

    Dictionary wrapper

---
+++ 193E - Acronym Expander/193E - Acronym Expander/AcroDict.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _193E___Acronym_Expander
+{
+    class AcroDict
+    {
+        private Dictionary<string, string> _acronyms;
+        public Dictionary<string, string> Acronyms
+        {
+            get { return _acronyms; }
+        }
+
+
+        public AcroDict()
+        {
+            _acronyms.Add("lol", "laugh out loud");
+            _acronyms.Add("dw", "don't worry");
+            _acronyms.Add("hf", "have fun");
+            _acronyms.Add("gg", "good game");
+            _acronyms.Add("brb", "be right back");
+            _acronyms.Add("g2g", "got to go");
+            _acronyms.Add("wtf", "what the fuck");
+            _acronyms.Add("wp", "well played");
+            _acronyms.Add("gl", "good luck");
+            _acronyms.Add("imo", "in my opinion");
+        }
+
+        public AcroDict(string csvTitle)
+        {
+            //TODO: Call dptools to open a CSV, load it into _acronyms
+        }
+    }
+}
